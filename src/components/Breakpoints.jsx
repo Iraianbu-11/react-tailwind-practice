17b15e83333706defe98c137b8ef58ff6ea28fce
Iraianbu <iraianbu011@gmail.com>
2024-11-26 15:17:18 +0530

    feat: Breakpoints completed

--- src/components/Breakpoints.jsx
+++ src/components/Breakpoints.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from "react";
 
 const Breakpoints = () => {
   return (
-    <div>Breakpoints</div>
-  )
-}
+    <>
+      <h1 className="text-center text-xl my-3">Breakpoints in Tailwind CSS</h1>
+      <h1 className="text-center text-xl my-3">Adjust the screen size to see changes</h1>
+      <div className="flex justify-center min-h-5">
+        <div className="bg-black p-10 w-[100px] h-[100px] sm:bg-blue-500 md:bg-pink-500 lg:bg-indigo-500 xl:bg-red-500"></div>
+      </div>
+    </>
+  );
+};
 
-export default Breakpoints
+export default Breakpoints;
